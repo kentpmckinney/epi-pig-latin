85ca2c8792e2abe575a4d996c9795beee17d9d3b
Kent McKinney <kent.p.mckinney@gmail.com>
2020-02-02 22:23:01 -0800

    adds regex

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ function translate(text) {
 
   console.log(`translate(${text})`);
 
-  return false;
+  let pig = text.replace(/(?:\b)(\w*?)([aeiou]+\w*?)(?:\b)/g, '$2$1ay');
+  console.log(pig);
+
+  return pig;
 
 
 }
@@ -12,6 +15,7 @@ $(document).ready(function(){
 
   $("#form").submit(function(e){
 
+    $("#result").empty();
     let text = $("#text").val();
     let span = document.createElement("span")
     span.innerText = `Translation: ${translate(text)}`;
