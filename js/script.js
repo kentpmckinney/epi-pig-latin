368e5def3b556c647407ade86e29b8ffc0649759
Kent McKinney <kent.p.mckinney@gmail.com>
2020-02-03 19:21:40 -0800

    refactors html

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 function translate(text) {
-
   return text.replace(/(?:\b)([^aeiou\s]*qu[^aeiou\s]*|[^aeiou\s]+?)?([aeiouy]+\w*?)(?:\b)/ig, function(m, c1, c2){
     return /^[aeiou]+/i.test(m) ? `${m}way` : `${c2}${c1}ay`;
   });
- 
 }
 
 $(document).ready(function(){
 
   $("#form").submit(function(e){
-
     $("#result").empty();
     let text = $("#text").val();
     let span = document.createElement("span")
     span.innerText = `Translation: ${translate(text)}`;
     $("#result").append(span);
     e.preventDefault();
-
   });
 
 });
